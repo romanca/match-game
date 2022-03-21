2529e1b7b5b4d616c89ffe57d98e3454d9f0d0b6
romanca <r.jamrisko@gmail.com>
2022-03-21 12:20:50 +0100

    code cleaning,counter title

--- src/SideBar.js
+++ src/SideBar.js
@@ -6,8 +6,8 @@ const SideBar = () => {
     <div className="sidebar_container">
       <div className="player1_container">
         <div className="player1_header">
-          <div style={{ width: "100%", fontWeight: "bold" }}>Player 1</div>
-          <div style={{ width: "100%", fontWeight: "bold" }}>SCORE:2</div>
+          <div className="header_titles">Player 1</div>
+          <div className="header_titles">SCORE:2</div>
         </div>
         <div className="player1_counters_container">
           <div className="player1_module-border-wrap">
@@ -15,17 +15,13 @@ const SideBar = () => {
               <div className="player1_pair_counter">0</div>
             </div>
           </div>
-          <div className="player1_module-border-wrap">
-            <div className="player1_module">
-              <div className="player1_pair_counter">0</div>
-            </div>
-          </div>
+          <div className="counter_title">found pairs</div>
         </div>
       </div>
       <div className="player2_container">
         <div className="player2_header">
-          <div style={{ width: "100%", fontWeight: "bold" }}>Player 2</div>
-          <div style={{ width: "100%", fontWeight: "bold" }}>SCORE:12</div>
+          <div className="header_titles">Player 2</div>
+          <div className="header_titles">SCORE:12</div>
         </div>
         <div className="player2_counters_container">
           <div className="player2_module-border-wrap">
@@ -33,20 +29,9 @@ const SideBar = () => {
               <div className="player2_pair_counter">0</div>
             </div>
           </div>
-          <div className="player2_module-border-wrap">
-            <div className="player2_module">
-              <div className="player2_pair_counter">0</div>
-            </div>
-          </div>
+          <div className="counter_title">found pairs</div>
         </div>
       </div>
-      {/* <div
-        style={{
-          width: "100%",
-          border: "1px solid black",
-          marginTop: "5px",
-        }}
-      ></div> */}
     </div>
   );
 };
